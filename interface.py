53639c22dbb573e2fc625ece489a43433f357558
Elliot Naylor <15021270@students.southwales.ac.uk>
2023-06-19 12:40:04 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -51,109 +51,109 @@ class MainApplication(tk.Tk, ABCD_Rules) :
         
         info_frame =tk.LabelFrame(container, text="Information")
         info_frame.grid(row=0,rowspan=3, column=0, padx=20, pady=20)
-
+        
         # Saving User Info
-        abcd_info_frame =tk.LabelFrame(info_frame, text="ABCD Rules")
+        abcd_info_frame =tk.LabelFrame(info_frame, text="ABCD Rules", highlightthickness=0, borderwidth = 0)
         abcd_info_frame.grid(row= 0, column=0, padx=20, pady=20)
 
         asymmetry_name_label = tk.Label(abcd_info_frame, text="Asymmetry")
-        asymmetry_name_label.grid(row=0, column=0, padx=10, pady=10)
+        asymmetry_name_label.grid(row=0, column=0, padx=10, pady=5)
         self.asymmetry_name_entry = tk.Entry(abcd_info_frame)
-        self.asymmetry_name_entry.grid(row=0, column=1, padx=10, pady=10)
+        self.asymmetry_name_entry.grid(row=0, column=1, padx=10, pady=5)
 
         border_name_label = tk.Label(abcd_info_frame, text="Border")
-        border_name_label.grid(row=1, column=0, padx=10, pady=10)
+        border_name_label.grid(row=1, column=0, padx=10, pady=5)
         border_name_entry = tk.Entry(abcd_info_frame)
-        border_name_entry.grid(row=1, column=1, padx=10, pady=10)
+        border_name_entry.grid(row=1, column=1, padx=10, pady=5)
 
         colour_name_label = tk.Label(abcd_info_frame, text="Colour")
-        colour_name_label.grid(row=2, column=0, padx=10, pady=10)
+        colour_name_label.grid(row=2, column=0, padx=10, pady=5)
         colour_name_entry = tk.Entry(abcd_info_frame)
-        colour_name_entry.grid(row=2, column=1, padx=10, pady=10)
+        colour_name_entry.grid(row=2, column=1, padx=10, pady=5)
 
 
         dermo_name_label = tk.Label(abcd_info_frame, text="Dermoscopic")
-        dermo_name_label.grid(row=3, column=0, padx=10, pady=10)
+        dermo_name_label.grid(row=3, column=0, padx=10, pady=5)
         self.dermo_name_entry = tk.Entry(abcd_info_frame, state="disabled")
-        self.dermo_name_entry.grid(row=3, column=1, padx=10, pady=10)
+        self.dermo_name_entry.grid(row=3, column=1, padx=10, pady=5)
 
         #Section on dermoscopic structures
-        structures_info_frame =tk.LabelFrame(info_frame, text="Dermoscopic Structures")
+        structures_info_frame =tk.LabelFrame(info_frame, text="Dermoscopic Structures", highlightthickness=0, borderwidth = 0)
         structures_info_frame.grid(row= 1, column=0, padx=20, pady=20)
 
         network_name_label = tk.Label(structures_info_frame, text="Pigment Network")
-        network_name_label.grid(row=0, column=0, padx=10, pady=10)
+        network_name_label.grid(row=0, column=0, padx=10, pady=5)
         self.network_combo = ttk.Combobox(structures_info_frame, values=DROP_DOWN_OPTIONS)
-        self.network_combo.grid(row=0, column=1, padx=10, pady=10)
+        self.network_combo.grid(row=0, column=1, padx=10, pady=5)
         
         negative_network_name_label = tk.Label(structures_info_frame, text="Negative Network")
-        negative_network_name_label.grid(row=1, column=0, padx=10, pady=10)
+        negative_network_name_label.grid(row=1, column=0, padx=10, pady=5)
         self.negative_network_combo = ttk.Combobox(structures_info_frame, values=DROP_DOWN_OPTIONS)
-        self.negative_network_combo.grid(row=1, column=1, padx=10, pady=10)
+        self.negative_network_combo.grid(row=1, column=1, padx=10, pady=5)
         
         streaked_name_label = tk.Label(structures_info_frame, text="Branched Streaks")
-        streaked_name_label.grid(row=2, column=0, padx=10, pady=10)
+        streaked_name_label.grid(row=2, column=0, padx=10, pady=5)
         self.streaked_combo = ttk.Combobox(structures_info_frame, values=DROP_DOWN_OPTIONS)
-        self.streaked_combo.grid(row=2, column=1, padx=10, pady=10)
+        self.streaked_combo.grid(row=2, column=1, padx=10, pady=5)
 
         milia_name_label = tk.Label(structures_info_frame, text="Milia-Like Cysts")
-        milia_name_label.grid(row=3, column=0, padx=10, pady=10)
+        milia_name_label.grid(row=3, column=0, padx=10, pady=5)
         self.milia_combo = ttk.Combobox(structures_info_frame, values=DROP_DOWN_OPTIONS)
-        self.milia_combo.grid(row=3, column=1, padx=10, pady=10)
+        self.milia_combo.grid(row=3, column=1, padx=10, pady=5)
 
         globules_name_label = tk.Label(structures_info_frame, text="Globules")
-        globules_name_label.grid(row=4, column=0, padx=10, pady=10)
+        globules_name_label.grid(row=4, column=0, padx=10, pady=5)
         self.globules_combo = ttk.Combobox(structures_info_frame, values=DROP_DOWN_OPTIONS)
-        self.globules_combo.grid(row=4, column=1, padx=10, pady=10)
+        self.globules_combo.grid(row=4, column=1, padx=10, pady=5)
 
         #Section on extra patient information including DOB, gender, and location of lesion
-        patient_info_frame =tk.LabelFrame(info_frame, text="Patient information")
+        patient_info_frame =tk.LabelFrame(info_frame, text="Patient information", highlightthickness=0, borderwidth = 0)
         patient_info_frame.grid(row= 2, column=0, padx=20, pady=20)
 
         dob_name_label = tk.Label(patient_info_frame, text="Date of Birth")
-        dob_name_label.grid(row=0, column=0, padx=20, pady=10)
+        dob_name_label.grid(row=0, column=0, padx=20, pady=5)
         dob_name_entry = tk.Entry(patient_info_frame)
-        dob_name_entry.grid(row=0, column=1, padx=10, pady=10)
+        dob_name_entry.grid(row=0, column=1, padx=10, pady=5)
 
         gender_name_label = tk.Label(patient_info_frame, text="Gender")
-        gender_name_label.grid(row=1, column=0, padx=20, pady=10)
+        gender_name_label.grid(row=1, column=0, padx=20, pady=5)
         gender_name_entry = tk.Entry(patient_info_frame)
-        gender_name_entry.grid(row=1, column=1, padx=10, pady=10)
+        gender_name_entry.grid(row=1, column=1, padx=10, pady=5)
 
         location_name_label = tk.Label(patient_info_frame, text="Body Location")
-        location_name_label.grid(row=2, column=0, padx=20, pady=10)
+        location_name_label.grid(row=2, column=0, padx=20, pady=5)
         location_name_entry = tk.Entry(patient_info_frame)
-        location_name_entry.grid(row=2, column=1, padx=10, pady=10)
+        location_name_entry.grid(row=2, column=1, padx=10, pady=5)
 
 
-        buttons_frame =tk.LabelFrame(info_frame, text="buttons")
+        buttons_frame =tk.LabelFrame(info_frame, text="buttons", highlightthickness=0, borderwidth = 0)
         buttons_frame.grid(row=3, column=0, padx=20, pady=20)
 
         #Loads dataset, will be removed in future update
         init_button = tk.Button(buttons_frame, text="Run", command=lambda : self.run())
-        init_button.grid(row=0, column=0, sticky="news", padx=20, pady=10)
+        init_button.grid(row=0, column=0, sticky="news", padx=20, pady=5)
 
         load_button = tk.Button(buttons_frame, text="load Image", command=lambda : self.load_image())
-        load_button.grid(row=0, column=1, sticky="news", padx=20, pady=10)
+        load_button.grid(row=0, column=1, sticky="news", padx=20, pady=5)
 
         lesion_info_frame = tk.LabelFrame(container, text="Patient information")
         lesion_info_frame.grid(row=1, column=1, padx=0, pady=20)
         lesion_name_label = tk.Label(lesion_info_frame, width=40, height=20, text="No data loaded")
-        lesion_name_label.grid(row=1, column=0, padx=20, pady=10)
+        lesion_name_label.grid(row=1, column=0, padx=20, pady=5)
 
         self.lesion_name_label = tk.Label(lesion_info_frame, text="No image loaded")
-        self.lesion_name_label.grid(row=1, column=0, padx=20, pady=10)
+        self.lesion_name_label.grid(row=1, column=0, padx=20, pady=5)
 
         more_info_button = tk.Button(lesion_info_frame, text="More info", command=lambda : self.show_segmentation())
-        more_info_button.grid(row = 2, column = 0, padx=20, pady=10)
+        more_info_button.grid(row = 2, column = 0, padx=20, pady=5)
 
         bayesian_info_frame = tk.LabelFrame(container, text="Bayesian")
         bayesian_info_frame.grid(row=1, column=2, padx=20, pady=20)
         bayesian_name_label = tk.Label(bayesian_info_frame, width=40, height=20, text="No data loaded")
-        bayesian_name_label.grid(row=1, column=0, padx=20, pady=10)
+        bayesian_name_label.grid(row=1, column=0, padx=20, pady=5)
 
         self.bayesian_name_label = tk.Label(bayesian_info_frame, text="No data loaded")
-        self.bayesian_name_label.grid(row=1, column=0, padx=20, pady=10)
+        self.bayesian_name_label.grid(row=1, column=0, padx=20, pady=5)
     
     def run(self):
         print("Run pressed")
